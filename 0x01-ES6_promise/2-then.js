75f57e2cbcaf53bc18ccc7206f30c78847f7bb66
Hamoodteen <ahmed104567@outlook.com>
2024-04-24 21:19:51 +0200

    0

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -3,9 +3,9 @@ export default function handleResponseFromAPI(promise) {
     // eslint-disable-next-line no-unused-expressions
     promise ? resolve({
       status: 200,
-      body: 'Success',
+      body: 'success',
     }) : reject(new Error(''));
   });
-  pr.then(() => { console.log('Got a response from the API'); });
+  pr.then(() => { console.log('Got a response from the API'); }, () => { console.log('Got a response from the API'); });
   return pr;
 }
