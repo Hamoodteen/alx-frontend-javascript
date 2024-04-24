d4625586f2cc1b3cb1c1d46da44f6c44c13518ea
Hamoodteen <ahmed104567@outlook.com>
2024-04-24 21:10:29 +0200

    0

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,10 +1,13 @@
 export default function getFullResponseFromAPI(success) {
   const pr = new Promise((resolve, reject) => {
-    // eslint-disable-next-line no-unused-expressions
-    success ? resolve({
-      status: 200,
-      body: 'success',
-    }) : reject(new Error('The fake API is not working currently'));
+    if (success) {
+      resolve({
+        status: 200,
+        body: 'success',
+      });
+    } else {
+      reject(new Error('The fake API is not working currently'));
+    }
   });
   return pr;
 }
