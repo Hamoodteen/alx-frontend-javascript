f5d6489034e565e828a292f274dc3cd4161ff795
Hamoodteen <ahmed104567@outlook.com>
2024-04-24 15:33:57 +0200

    0

--- 0x00-ES6_basic/3-default-parameter.js
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -1,2 +1,4 @@
 // eslint-disable-next-line max-len
-export default (initialNumber, expansion1989 = undefined, expansion2019 = undefined) => initialNumber + expansion1989 + expansion2019;
+export default function getSumOfHoods(initialNumber, expansion1989 = undefined, expansion2019 = undefined) {
+  return initialNumber + expansion1989 + expansion2019;
+}
