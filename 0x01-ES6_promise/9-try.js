a6cfab24ca7c0e148209c7b5d2acb7cefc877caf
Hamoodteen <ahmed104567@outlook.com>
2024-04-24 22:20:40 +0200

    0

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -2,9 +2,9 @@ export default function guardrail(mathFunction) {
   // eslint-disable-next-line prefer-const
   let queue = [];
   try {
-    queue.push(mathFunction);
+    queue.push(mathFunction());
   } catch (e) {
-    queue.push(e);
+    queue.push(`Error: ${e.message}`);
   } finally {
     queue.push('Guardrail was processed');
   }
