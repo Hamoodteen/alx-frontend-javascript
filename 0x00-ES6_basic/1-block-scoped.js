befc8cdf790e1921e285b1e85902557d0ab5f393
Hamoodteen <ahmed104567@outlook.com>
2024-04-24 15:18:04 +0200

    0

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,7 +3,9 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse) {
+    // eslint-disable-next-line no-const-assign
     task = true;
+    // eslint-disable-next-line no-const-assign
     task2 = false;
   }
 
