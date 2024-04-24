e078aff9afee544105720b5581c16f5d4be4f454
Hamoodteen <ahmed104567@outlook.com>
2024-04-24 18:39:37 +0200

    0

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,8 +1,3 @@
 export default function iterateThroughObject(reportWithIterator) {
-  let st = '';
-  for (const iterator of reportWithIterator) {
-    st += `${iterator} | `;
-  }
-  const res = st.slice(0, -3);
-  return res;
+  return reportWithIterator.join(' | ');
 }
