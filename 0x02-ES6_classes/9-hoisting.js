64111e207924f36893b48b7aa0d0ffb258490b4a
Hamoodteen <ahmed104567@outlook.com>
2024-04-25 15:36:13 +0200

    0

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -19,9 +19,10 @@ const class2019 = new HolbertonClass(2019, 'San Francisco');
 const class2020 = new HolbertonClass(2020, 'San Francisco');
 
 export class StudentHolberton {
-  constructor(firstName, lastName) {
+  constructor(firstName, lastName, holbertonClass) {
     this._firstName = firstName;
     this._lastName = lastName;
+    this._holbertonClass = holbertonClass;
   }
 
   get fullName() {
@@ -34,7 +35,7 @@ export class StudentHolberton {
 
   // eslint-disable-next-line class-methods-use-this
   get fullStudentDescription() {
-    return `${this._firstName} ${this._lastName} - ${this._holbertonClass.year} - ${this._holbertonClass.location}`;
+    return `${this._firstName} ${this._lastName} - ${this._holbertonClass}`;
   }
 }
 
