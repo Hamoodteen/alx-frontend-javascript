55f9d2a170f86326c6a2b98ac358a3deca85774c
Hamoodteen <ahmed104567@outlook.com>
2024-04-25 14:07:04 +0200

    0

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -2,9 +2,9 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
     if (typeof name !== 'string') {
-      throw new Error('TypeError: Name must be a string');
+      throw new TypeError('Name must be a string');
     } else if (typeof length !== 'number') {
-      throw new Error('TypeError: Length must be a number');
+      throw new TypeError('Length must be a number');
     }
     this._name = name;
     this._length = length;
@@ -25,14 +25,14 @@ export default class HolbertonCourse {
 
   set name(value) {
     if (typeof value !== 'string') {
-      throw new Error('TypeError: Name must be a string');
+      throw new TypeError('Name must be a string');
     }
     this._name = value;
   }
 
   set length(value) {
     if (typeof value !== 'number') {
-      throw new Error('TypeError: Length must be a number');
+      throw new TypeError('Length must be a number');
     }
     this._length = value;
   }
