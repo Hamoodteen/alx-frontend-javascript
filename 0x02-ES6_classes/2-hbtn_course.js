7d67845383bd000ece2257039c035591abd458ef
Hamoodteen <ahmed104567@outlook.com>
2024-04-25 14:03:29 +0200

    0

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,37 @@
+/* eslint-disable no-underscore-dangle */
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    if (typeof name !== 'string') {
+      throw new Error('TypeError: Name must be a string');
+    } else if (typeof length !== 'number') {
+      throw new Error('TypeError: Length must be a number');
+    }
+    this._name = name;
+    this._length = length;
+    this._students = students;
+    get name() {
+        return this._name;
+    }
+    get length() {
+        return this._length;
+    }
+    get students() {
+        return this._students;
+    }
+    set name(value) {
+        if (typeof value !== 'string') {
+            throw new Error('TypeError: Name must be a string');
+        }
+        this._name = value;
+    }
+    set length(value) {
+        if (typeof value !== 'number') {
+            throw new Error('TypeError: Length must be a number');
+          }
+        this._length = value;
+    }
+    set students(value) {
+        this._students = value;
+    }
+  }
+}
