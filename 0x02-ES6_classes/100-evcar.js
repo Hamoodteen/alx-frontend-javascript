ee28acf4c8a69ad82f90a2b56a8b355d44135a0d
Hamoodteen <ahmed104567@outlook.com>
2024-04-25 18:26:44 +0200

    0

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -6,4 +6,8 @@ export default class EVCar extends Car {
     super(brand, motor, color);
     this._range = range;
   }
+
+  cloneCar() {
+    return new Car(this._brand, this._motor, this._color);
+  }
 }
